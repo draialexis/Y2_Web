b5a89da2096ebf4a586e539922e4ace8e5ba8d80
draialexis <drai.alexis@gmail.com>
2022-03-20 21:53:04 +0100

    Do #11: implement 'submit' button disabled/enabled

--- www/js/login.js
+++ www/js/login.js
@@ -21,62 +21,74 @@ function addAllEvents() {
     const userPwd = document.getElementById("userpwd");
     const userEmail = document.getElementById("useremail");
 
-    lastName.addEventListener('input', checkNoEmpty);
-    firstName.addEventListener('input', checkNoEmpty);
-    birthDate.addEventListener('input', checkDate);
-    userName.addEventListener('input', checkUserName);
-    userPwd.addEventListener('input', checkPwd);
-    userEmail.addEventListener('input', checkEmail);
-
-    function validate(elm) {
-        elm.classList.remove(invalidClass);
-        elm.classList.add(validClass);
-    }
-
-    function invalidate(elm) {
-        elm.classList.remove(validClass);
-        elm.classList.add(invalidClass);
-    }
-
-    function checkSize(elm, minSize) {
-        if (elm.value.length >= minSize && elm.classList.contains(invalidClass)) {
-            validate(elm);
-        } else if (elm.value.length < minSize && !(elm.classList.contains(invalidClass))) {
-            invalidate(elm);
-        }
-    }
-
-    function checkFormat(elm, inFormat) {
-        if (elm.value.match(inFormat) && elm.classList.contains(invalidClass)) {
-            validate(elm);
-        } else if (!(elm.value.match(inFormat)) && !(elm.classList.contains(invalidClass))) {
-            invalidate(elm);
-        }
-    }
+    [checkNoEmpty, checkAll].forEach(fun =>
+        lastName.addEventListener("input", fun, false)
+    );
+    [checkNoEmpty, checkAll].forEach(fun =>
+        firstName.addEventListener("input", fun, false)
+    );
+    [checkDate, checkAll].forEach(fun =>
+        birthDate.addEventListener("input", fun, false)
+    );
+    [checkUserName, checkAll].forEach(fun =>
+        userName.addEventListener("input", fun, false)
+    );
+    [checkPwd, checkAll].forEach(fun =>
+        userPwd.addEventListener("input", fun, false)
+    );
+    [checkEmail, checkAll].forEach(fun =>
+        userEmail.addEventListener("input", fun, false)
+    );
 
     function checkNoEmpty(e) {
-        checkSize(e.target, 1);
+        return check(e.target, 1, e.target.value.length >= 1);
     }
 
     function checkDate(e) {
-        checkFormat(e.target, dobExp);
+        return check(e.target, dobExp, e.target.value.match(dobExp));
     }
 
     function checkUserName(e) {
-        checkSize(e.target, 6);
+        return check(e.target, 6, e.target.value.length >= 6);
     }
 
     function checkPwd(e) {
-        checkFormat(e.target, pwdExp);
+        return check(e.target, pwdExp, e.target.value.match(pwdExp));
     }
 
     function checkEmail(e) {
-        checkFormat(e.target, emailExp);
+        return check(e.target, emailExp, e.target.value.match(emailExp));
     }
 
-    function checkAll(e) {
+    function check(elm, inFormat, condition) {
+        if (condition && elm.classList.contains(invalidClass)) {
+            elm.classList.remove(invalidClass);
+            elm.classList.add(validClass);
+            return true;
+        } else if (!condition && !(elm.classList.contains(invalidClass))) {
+            elm.classList.remove(validClass);
+            elm.classList.add(invalidClass);
+            return false;
+        }
+    }
 
+    function checkAll() {
+        let button = document.getElementById("login-submit");
+        if (
+            lastName.value.length >= 1
+            && firstName.value.length >= 1
+            && birthDate.value.match(dobExp)
+            && userName.value.length >= 6
+            && userPwd.value.match(pwdExp)
+            && userEmail.value.match(emailExp)
+        ) {
+            if (button.disabled === true) {
+                button.disabled = false;
+            }
+        } else {
+            if (button.disabled === false) {
+                button.disabled = true;
+            }
+        }
     }
 }
-
-
