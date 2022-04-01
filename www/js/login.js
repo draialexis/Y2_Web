47a1ccf96846b1b733b6d61d2184097d0ea92174
draialexis <draialexis@gmail.com>
2022-04-01 10:50:37 +0200

    Fix #23

---
+++ www/js/login.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    addAllEvents();
+
+}, false);
+
+function addAllEvents() {
+
+    const userName = document.getElementById("username");
+    const userPwd = document.getElementById("userpwd");
+
+    const form = document.getElementById("login-form");
+
+    form.addEventListener("submit", function (e) {
+        if (/*...*/) {
+            e.preventDefault();
+            // ...
+        } else {
+            form.submit();
+        }
+    });
+}
