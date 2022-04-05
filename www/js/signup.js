173238525a68c8008b4ce6d936b9d53708b39bb3
Alexis DRAI <drai.alexis@gmail.com>
2022-04-05 18:14:50 +0200

    Work on #18

--- www/js/signup.js
+++ www/js/signup.js
@@ -16,38 +16,44 @@ function addAllEvents() {
     const userName = document.getElementById("username");
     const userPwd = document.getElementById("userpwd");
     const userEmail = document.getElementById("useremail");
+
     const form = document.getElementById("signup-form");
+    let button = document.getElementById("signup-submit");
 
-    [checkNoEmpty, checkAll].forEach(fun =>
-        lastName.addEventListener("input", fun)
-    );
-    [checkNoEmpty, checkAll].forEach(fun =>
-        firstName.addEventListener("input", fun)
-    );
-    [checkDate, checkAll].forEach(fun =>
-        birthDate.addEventListener("input", fun)
-    );
-    [checkUserName, checkAll].forEach(fun =>
-        userName.addEventListener("input", fun)
-    );
-    [checkPwd, checkAll].forEach(fun =>
-        userPwd.addEventListener("input", fun)
-    );
-    [checkEmail, checkAll].forEach(fun =>
-        userEmail.addEventListener("input", fun)
+    const inputFields = [lastName, firstName, birthDate, userName, userPwd, userEmail];
+
+    inputFields.forEach(field =>
+        field.addEventListener("keyup", checkAll)
     );
 
-    function checkNoEmpty(e) {
-        return check(e.target, e.target.value.length >= 1);
+    [lastName, firstName].forEach(field =>
+        field.addEventListener("keyup", checkNoEmpty)
+    );
+
+    birthDate.addEventListener("keyup", checkDate);
+    userName.addEventListener("keyup", checkUserName);
+    userPwd.addEventListener("keyup", checkPwd);
+    userEmail.addEventListener("keyup", checkEmail);
+
+    function allowedValues(fieldName, value) {
+        switch (fieldName) {
+            case("names"):
+                return value.length >= 1;
+            case("birthdate"):
+                return value === "" || value === null || parseMyDate(value);
+            case("username"):
+                return value.length >= 6;
+            case("userpwd"):
+                return value.match(pwdExp);
+            case("useremail"):
+                return value.match(emailExp);
+        }
     }
 
     function parseMyDate(elm) {
         const now = new Date();
         const arr = elm.split("/");
         const date = new Date(arr[2], arr[1] - 1, arr[0]);
-        console.log("date=" + date.getTime());
-        console.log("now=" + now.getTime());
-        console.log("date<=now:" + date.getTime() <= now.getTime());
         return (
             date.getMilliseconds() === date.getMilliseconds()   // true if the date was JS-valid
             && arr[0] >= 1 && arr[0] <= 31                      // day is possible (not accounting for different months)
@@ -56,20 +62,24 @@ function addAllEvents() {
         );
     }
 
+    function checkNoEmpty(e) {
+        return check(e.target, allowedValues("names", e.target.value));
+    }
+
     function checkDate(e) {
-        return check(e.target, parseMyDate(e.target.value) || e.target.value === "" || e.target.value === null);
+        return check(e.target, allowedValues("birthdate", e.target.value));
     }
 
     function checkUserName(e) {
-        return check(e.target, e.target.value.length >= 6);
+        return check(e.target, allowedValues("username", e.target.value));
     }
 
     function checkPwd(e) {
-        return check(e.target, e.target.value.match(pwdExp));
+        return check(e.target, allowedValues("userpwd", e.target.value));
     }
 
     function checkEmail(e) {
-        return check(e.target, e.target.value.match(emailExp));
+        return check(e.target, allowedValues("useremail", e.target.value));
     }
 
     function check(elm, condition) {
@@ -78,27 +88,26 @@ function addAllEvents() {
                 elm.classList.remove(invalidClass);
             }
             elm.classList.add(validClass);
-            elm.nextElementSibling.hidden = true; // displaying warnings
+            elm.nextElementSibling.hidden = true; // hiding warnings
             return true;
         } else if (!condition) {
             if (elm.classList.contains(validClass)) {
                 elm.classList.remove(validClass);
             }
             elm.classList.add(invalidClass);
-            elm.nextElementSibling.hidden = false; // hiding any warnings
+            elm.nextElementSibling.hidden = false; // displaying warnings
             return false;
         }
     }
 
     function checkAll() {
-        let button = document.getElementById("signup-submit");
         if (
-            lastName.value.length >= 1
-            && firstName.value.length >= 1
-            && (birthDate.value === "" || birthDate.value === null || parseMyDate(birthDate.value))
-            && userName.value.length >= 6
-            && userPwd.value.match(pwdExp)
-            && userEmail.value.match(emailExp)
+            allowedValues("names", lastName.value)
+            && allowedValues("names", firstName.value)
+            && allowedValues("birthdate", birthDate.value)
+            && allowedValues("username", userName.value)
+            && allowedValues("userpwd", userPwd.value)
+            && allowedValues("useremail", userEmail.value)
         ) { // all good ...
             if (button.disabled === true) { // ... disabled => enable
                 button.disabled = false;
