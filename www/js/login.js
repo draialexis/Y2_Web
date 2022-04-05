daa0faa10248a7b0f1c9af7c968558997b2fad9b
Alexis DRAI <drai.alexis@gmail.com>
2022-04-05 16:54:19 +0200

    Fix #25

--- www/js/login.js
+++ www/js/login.js
@@ -7,6 +7,7 @@ function addAllEvents() {
 
     const userName = document.getElementById("username");
     const userPwd = document.getElementById("userpwd");
+    const form = document.getElementById("login-form");
 
     [userName, userPwd].forEach(input =>
         input.addEventListener("keyup", function (e) {
@@ -16,4 +17,34 @@ function addAllEvents() {
             }
         })
     );
+
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        // could be made more dynamic, and less dependent on details in the html... maybe using FormData?
+        // on the other hand, no time + login implementation details are fairly standard
+        const params = "username=" + userName.value + "&userpwd=" + userPwd.value;
+        const url = "htbin/login.py";
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send(params);
+
+        const target = document.getElementById("ajax-target");
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = xhr.responseText;
+                target.innerHTML = response;
+                target.hidden = false;
+                if (response.startsWith("Bonjour")) {
+                    target.style.color = "darkgreen";
+                } else {
+                    target.style.color = "darkred";
+                }
+            }
+        }
+    });
+    //TODO reset should clear all borders and warnings
 }
