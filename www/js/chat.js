75752feff0db37b5d14ffd22762ea560b176c2f8
Alexis DRAI <drai.alexis@gmail.com>
2022-04-14 17:00:59 +0200

    Bleeeeuuuargh

--- www/js/chat.js
+++ www/js/chat.js
@@ -1,22 +1,37 @@
 /* By Matrod C. & Drai A., 2021-22 */
 
 $(document).ready(function () {
-
-    const form = $("#chat-form");
-
-    $("#chat-input").keyup(function (evt) {
-        if (evt.which === 13) {
-            evt.preventDefault();
-            $("#chat-form").submit();
-        }
-    });
+    const $form = $("#chat-form");
+    const $box = $("#comments-section");
+    const postUrl = "htbin/chatsend.py";
+    const getUrl = "htbin/chatget.py";
+    const data = $form.serialize();
+    refresh();
 
     $("#chat-toggle").click(function () {
         $(this).toggleClass("untoggled");
-        if (form.attr("hidden")) {
-            form.removeAttr("hidden");
+        if ($form.attr("hidden")) {
+            $form.removeAttr("hidden");
         } else {
-            form.attr("hidden", true);
+            $form.attr("hidden", true);
         }
     });
+
+    $("#chat-submit").click(function (evt) {
+        evt.preventDefault();
+        $.post(postUrl, data)
+            .done(function (data, status) {
+                console.log("Data: " + data + "\nStatus: " + status)
+            });
+        refresh();
+    });
+
+    function refresh() {
+        $box.empty();
+        $.get(getUrl, function (data) {
+            data.forEach(datum => {
+                $box.append(datum.date + ", " + datum.time + " (" + datum.user + ") : " + datum.msg + "<br/>");
+            })
+        });
+    }
 });
